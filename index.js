2d9dcf024021dc574a73a4755ebf88992d31ef5d
Cubin <78607391+NiceCubin@users.noreply.github.com>
2021-09-13 17:08:33 +0000

    updated discord.js to v13.1.0

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const keepAlive = require("./server");
 
-const client = new Discord.Client();
+const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
   
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
