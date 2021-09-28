d7d9611cc81c84a6d6be99d0e8d30b438459843d
Cubin <78607391+NiceCubin@users.noreply.github.com>
2021-09-28 07:42:10 +0000

    deleted unnecessary stuff

--- src/utils/default.js
+++ src/utils/default.js
@@ -1,9 +1,8 @@
 module.exports = {
+  getEmojiImage: emojiID => `https://cdn.discordapp.com/emojis/${emojiID}.png`,
   cleanText(text) {
     let formatChars = ["\\", "*", "_", "`", ">"];
     formatChars.forEach(char => text = text.replaceAll(char, `\\${char}`));
     return text;
-  },
-  
-  getEmojiImage(emojiID) {`https://cdn.discordapp.com/emojis/${emojiID}.png`;}
+  }
 }
--- src/utils/embeds.js
+++ src/utils/embeds.js
@@ -1,39 +1,7 @@
-const { MessageEmbed } = require("discord.js")
-
-const embed = new MessageEmbed()
-
-function error(description) {
-  return embed
-    .setTitle("Error...")
-    .setDescription(description)
-    .setColor("RED")
-}
-
-function win(description) {
-  return embed
-    .setTitle("You Win!")
-    .setDescription(description)
-    .setColor(0x00ff00)
-}
-
-function lose(description) {
-  return embed
-    .setTitle("You Lose...")
-    .setDescription(description)
-    .setColor(0xff0000)
-}
-
-function timeout(description) {
-  return embed
-    .setTitle("Timeout!")
-    .setDescription(description ?? "You took too long to respond!")
-    .setColor("RED")
-}
-
 module.exports = {
   invalid: description => ({title: "Oops!", description, color: "RED"}),
   error: description => ({title: "Error...", description, color: "RED"}),
-    win: description => ({title: "You Won!", description, color: 0x00ff00}),
-    lose: description => ({title: "You Lost...", description, color: 0xff0000}),
-    timeout: description => ({title: "Timeout!", description, color: "RED"})
+  win: description => ({title: "You Won!", description, color: 0x00ff00}),
+  lose: description => ({title: "You Lost...", description, color: 0xff0000}),
+  timeout: description => ({title: "Timeout!", description, color: "RED"})
 }
