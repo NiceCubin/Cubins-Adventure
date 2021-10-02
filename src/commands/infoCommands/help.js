7870dd48e8849562da5a4855f95fdeb7a011ee4b
Cubin <78607391+NiceCubin@users.noreply.github.com>
2021-10-02 23:35:27 +0700

    Update help.js
    fixed indentation for command help description

--- src/commands/infoCommands/help.js
+++ src/commands/infoCommands/help.js
@@ -55,10 +55,10 @@ module.exports = new Command({
           {
             title: `Command: \`${client.prefix}${command.name}\``,
             description: dedent
-                         `**Description:** ${command.description}
-                         **Aliases:** \`${command.triggers.join(', ')}\`
-                         **Cooldown:** ${command.cooldown === 0 ? 'none' : command.cooldown}${command.cooldown === 0 ? '' : ` Second${command.cooldown === 1 ? '' : 's'}`}
-                         ${command.permissions.length === 0 ? '' : `**Permissions Needed:** \`${command.permissions.map(perm => getCamelCase(perm.replaceAll('_', ' ')))}\``}`,
+              `**Description:** ${command.description}
+              **Aliases:** \`${command.triggers.join(', ')}\`
+              **Cooldown:** ${command.cooldown === 0 ? 'none' : command.cooldown}${command.cooldown === 0 ? '' : ` Second${command.cooldown === 1 ? '' : 's'}`}
+              ${command.permissions.length === 0 ? '' : `**Permissions Needed:** \`${command.permissions.map(perm => getCamelCase(perm.replaceAll('_', ' ')))}\``}`,
             color: 0xff00ff,
             fields: [
               { name: 'Usage:', value: `\`${client.prefix}${command.name}${command.usage != null ? ` ${command.usage}` : ''}\`` }
