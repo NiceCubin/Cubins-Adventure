25f7d12715b78dbeb56ee7469cd22c0bcbadb2b5
Cubin <78607391+NiceCubin@users.noreply.github.com>
2021-10-13 14:11:51 +0000

    deleted say.js and renamed reload.js to reloadall.js

---
+++ src/commands/devCommands/reloadall.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+const Command = require('../../structures/Command');
+const embeds = require('../../utils/embeds');
+
+module.exports = new Command({
+  triggers: ['reloadall'],
+  description: 'reloads all commands.',
+  cooldown: 0,
+  usage: '<command>',
+  permissions: [],
+  devOnly: true,
+  async run(message, args, command, client) {
+    client.commands.clear()
+    fs.readdirSync('./src/commands').forEach(cat => {
+      fs.readdirSync(`./src/commands/${cat}`).forEach(cmd => {
+        delete require.cache[require.resolve(`../../commands/${cat}/${cmd}`)];
+        
+        command = require(`../../commands/${cat}/${cmd}`);
+        client.commands.set(cmd.name, cmd);
+      });
+    });
+    
+    return await message.reply('Successfully reloaded all commands.');
+    }
+});
